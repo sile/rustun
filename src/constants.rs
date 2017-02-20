5001356b53cd404d447705a568fcf34e435e6dc4
Takeru Ohta <phjgt308@gmail.com>
2017-02-21 03:15:23 +0900

    Add some documentation

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,19 @@
+//! Constants related to STUN.
+
+/// The magic cookie value.
+///
+/// > The magic cookie field **MUST** contain the fixed value `0x2112A442` in
+/// > network byte order.
+/// > In [RFC 3489](https://tools.ietf.org/html/rfc3489), this field was part of
+/// > the transaction ID; placing the magic cookie in this location allows
+/// > a server to detect if the client will understand certain attributes
+/// > that were added in this revised specification.  In addition, it aids
+/// > in distinguishing STUN packets from packets of other protocols when
+/// > STUN is multiplexed with those other protocols on the same port.
+/// >
+/// > ([RFC 5389 -- 6. STUN Message Structure](https://tools.ietf.org/html/rfc5389#section-6))
 pub const MAGIC_COOKIE: u32 = 0x2112A442;
+
 pub const DEFAULT_PORT: u16 = 3478;
 pub const DEFAULT_MAX_MESSAGE_SIZE: usize = 548;
 pub const DEFAULT_RTO_MS: u64 = 500;
--- src/types.rs
+++ src/types.rs
@@ -1,9 +1,17 @@
-#[derive(Debug, Clone, Copy, PartialOrd, Ord, PartialEq, Eq, Hash)]
+//! Miscellaneous types.
+
+/// Unsigned 12 bit integer.
+#[derive(Debug, Default, Clone, Copy, PartialOrd, Ord, PartialEq, Eq, Hash)]
 pub struct U12(u16);
 impl U12 {
+    /// Converts from `u8` value.
     pub fn from_u8(value: u8) -> Self {
         U12(value as u16)
     }
+
+    /// Tries to convert from `u16` value.
+    ///
+    /// If `value` is greater than `0xFFF`, this will return `None`.
     pub fn from_u16(value: u16) -> Option<Self> {
         if value < 0x1000 {
             Some(U12(value))
@@ -11,13 +19,25 @@ impl U12 {
             None
         }
     }
+
+    /// Converts to `u16` value.
     pub fn as_u16(&self) -> u16 {
         self.0
     }
 }
 
+/// Transaction ID.
+///
+/// > STUN is a client-server protocol.  It supports two types of
+/// > transactions.  One is a request/response transaction in which a
+/// > client sends a request to a server, and the server returns a
+/// > response.  The second is an indication transaction in which either
+/// > agent -- client or server -- sends an indication that generates no
+/// > response.  Both types of transactions include a **transaction ID**, which
+/// > is a randomly selected 96-bit number.  For request/response
+/// > transactions, this transaction ID allows the client to associate the
+/// > response with the request that generated it; for indications, the
+/// > transaction ID serves as a debugging aid.
+/// >
+/// > ([RFC 5389 -- 3. Overview of Operation](https://tools.ietf.org/html/rfc5389#section-3))
 pub type TransactionId = [u8; 12];
-
-// TODO: delete(?)
-#[derive(Debug)]
-pub struct Unused(());
