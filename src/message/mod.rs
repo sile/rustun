bfa8e745e801d409efb5688eee59c5d036f3d12c
Takeru Ohta <phjgt308@gmail.com>
2017-02-26 19:13:11 +0900

    Add documentation for `message` module.

--- src/message/mod.rs
+++ src/message/mod.rs
@@ -1,3 +1,27 @@
+//! STUN message related components.
+//!
+//! > STUN is a client-server protocol.  It supports two types of
+//! > transactions. One is a **request/response** transaction in which a
+//! > client sends a **request** to a server, and the server returns a
+//! > **response**. The second is an **indication** transaction in which either
+//! > agent -- client or server -- sends an indication that generates no
+//! > response. Both types of transactions include a transaction ID, which
+//! > is a randomly selected 96-bit number.  For **request/response**
+//! > transactions, this transaction ID allows the client to associate the
+//! > response with the request that generated it; for indications, the
+//! > transaction ID serves as a debugging aid.
+//! >
+//! > All STUN messages start with a fixed header that includes a method, a
+//! > class, and the transaction ID.  The method indicates which of the
+//! > various requests or indications this is; this specification defines
+//! > just one method, Binding, but other methods are expected to be
+//! > defined in other documents.  The class indicates whether this is a
+//! > **request**, a **success response**, an **error response**, or an **indication**.
+//! > Following the fixed header comes zero or more attributes, which are
+//! > Type-Length-Value extensions that convey additional information for
+//! > the specific message.
+//! >
+//! > [RFC 5389 -- 3. Overview of Operation](https://tools.ietf.org/html/rfc5389#section-3)
 use rand;
 
 use {Method, Attribute};
@@ -8,8 +32,10 @@ pub use self::raw::{RawMessage, Class};
 
 mod raw;
 
+/// Response message.
 pub type Response<M, A> = Result<SuccessResponse<M, A>, ErrorResponse<M, A>>;
 
+/// Request message.
 #[derive(Debug, Clone)]
 pub struct Request<M, A> {
     method: M,
@@ -20,6 +46,7 @@ impl<M, A> Request<M, A>
     where M: Method,
           A: Attribute
 {
+    /// Makes a new request message.
     pub fn new(method: M) -> Self
         where M: Requestable
     {
@@ -29,26 +56,40 @@ impl<M, A> Request<M, A>
             attributes: Vec::new(),
         }
     }
+
+    /// Adds `attribute` to the tail of the attributes of this message.
     pub fn add_attribute<T: Into<A>>(&mut self, attribute: T) -> &mut Self {
         self.attributes.push(attribute.into());
         self
     }
+
+    /// Adds `attribute` to the tail of the attributes of this message.
     pub fn with_attribute<T: Into<A>>(mut self, attribute: T) -> Self {
         self.add_attribute(attribute);
         self
     }
+
+    /// Returns the method of this message.
     pub fn method(&self) -> &M {
         &self.method
     }
+
+    /// Returns the transaction ID of this message.
     pub fn transaction_id(&self) -> &TransactionId {
         &self.transaction_id
     }
+
+    /// Returns the attributes of this message.
     pub fn attributes(&self) -> &[A] {
         &self.attributes
     }
+
+    /// Converts into a success response message.
     pub fn into_success_response(self) -> SuccessResponse<M, A> {
         SuccessResponse::new(self.method, self.transaction_id)
     }
+
+    /// Converts into an error response message.
     pub fn into_error_response<E>(self, error_code: E) -> ErrorResponse<M, A>
         where E: Into<ErrorCode>
     {
@@ -56,6 +97,7 @@ impl<M, A> Request<M, A>
     }
 }
 
+/// Indication message.
 #[derive(Debug, Clone)]
 pub struct Indication<M, A> {
     method: M,
@@ -66,6 +108,7 @@ impl<M, A> Indication<M, A>
     where M: Method,
           A: Attribute
 {
+    /// Makes a new indication message.
     pub fn new(method: M) -> Self
         where M: Indicatable
     {
@@ -75,25 +118,38 @@ impl<M, A> Indication<M, A>
             attributes: Vec::new(),
         }
     }
+
+    /// Adds `attribute` to the tail of the attributes of this message.
     pub fn add_attribute<T: Into<A>>(&mut self, attribute: T) -> &mut Self {
         self.attributes.push(attribute.into());
         self
     }
+
+    /// Adds `attribute` to the tail of the attributes of this message.
     pub fn with_attribute<T: Into<A>>(mut self, attribute: T) -> Self {
         self.add_attribute(attribute);
         self
     }
+
+    /// Returns the method of this message.
     pub fn method(&self) -> &M {
         &self.method
     }
+
+    /// Returns the transaction ID of this message.
     pub fn transaction_id(&self) -> &TransactionId {
         &self.transaction_id
     }
+
+    /// Returns the attributes of this message.
     pub fn attributes(&self) -> &[A] {
         &self.attributes
     }
 }
 
+/// Success response message.
+///
+/// This is usually created by calling `Request::into_success_response` method.
 #[derive(Debug, Clone)]
 pub struct SuccessResponse<M, A> {
     method: M,
@@ -111,62 +167,81 @@ impl<M, A> SuccessResponse<M, A>
             attributes: Vec::new(),
         }
     }
+
+    /// Adds `attribute` to the tail of the attributes of this message.
     pub fn add_attribute<T: Into<A>>(&mut self, attribute: T) -> &mut Self {
         self.attributes.push(attribute.into());
         self
     }
+
+    /// Adds `attribute` to the tail of the attributes of this message.
     pub fn with_attribute<T: Into<A>>(mut self, attribute: T) -> Self {
         self.add_attribute(attribute);
         self
     }
+
+    /// Returns the method of this message.
     pub fn method(&self) -> &M {
         &self.method
     }
+
+    /// Returns the transaction ID of this message.
     pub fn transaction_id(&self) -> &TransactionId {
         &self.transaction_id
     }
+
+    /// Returns the attributes of this message.
     pub fn attributes(&self) -> &[A] {
         &self.attributes
     }
 }
 
+/// Error response message.
+///
+/// This is usually created by calling `Request::into_error_response` method.
 #[derive(Debug, Clone)]
 pub struct ErrorResponse<M, A> {
     method: M,
     transaction_id: TransactionId,
-    error_code: ErrorCode,
     attributes: Vec<A>,
 }
 impl<M, A> ErrorResponse<M, A>
     where M: Method,
           A: Attribute
 {
-    fn new(method: M, transaction_id: TransactionId, error_code: ErrorCode) -> Self {
+    fn new(method: M, transaction_id: TransactionId, _error_code: ErrorCode) -> Self {
+        // TODO: handle `_error_code`
         ErrorResponse {
             method: method,
             transaction_id: transaction_id,
-            error_code: error_code,
             attributes: Vec::new(),
         }
     }
+
+    /// Adds `attribute` to the tail of the attributes of this message.
     pub fn add_attribute<T: Into<A>>(&mut self, attribute: T) -> &mut Self {
         self.attributes.push(attribute.into());
         self
     }
+
+    /// Adds `attribute` to the tail of the attributes of this message.
     pub fn with_attribute<T: Into<A>>(mut self, attribute: T) -> Self {
         self.add_attribute(attribute);
         self
     }
+
+    /// Returns the method of this message.
     pub fn method(&self) -> &M {
         &self.method
     }
+
+    /// Returns the transaction ID of this message.
     pub fn transaction_id(&self) -> &TransactionId {
         &self.transaction_id
     }
+
+    /// Returns the attributes of this message.
     pub fn attributes(&self) -> &[A] {
         &self.attributes
-    }
-    pub fn error_code(&self) -> &ErrorCode {
-        &self.error_code
     }
 }
--- src/message/raw.rs
+++ src/message/raw.rs
@@ -1,3 +1,4 @@
+use std::mem;
 use std::io::{self, Read, Write};
 use handy_async::sync_io::{ReadExt, WriteExt};
 
@@ -7,6 +8,125 @@ use attribute::RawAttribute;
 use message::{Request, Indication, SuccessResponse, ErrorResponse, Response};
 use constants::MAGIC_COOKIE;
 
+/// The raw representation of a STUN message.
+///
+/// It is possible to perform conversion between an instance and
+/// the corresponding bytes without loss of information.
+///
+/// # NOTE: Binary Format of STUN Messages
+///
+/// > STUN messages are encoded in binary using network-oriented format
+/// > (most significant byte or octet first, also commonly known as big-
+/// > endian).  The transmission order is described in detail in Appendix B
+/// > of RFC 791 [RFC0791].  Unless otherwise noted, numeric constants are
+/// > in decimal (base 10).
+/// >
+/// > All STUN messages MUST start with a 20-byte header followed by zero
+/// > or more Attributes.  The STUN header contains a STUN message type,
+/// > magic cookie, transaction ID, and message length.
+/// >
+/// > ```text
+/// >  0                   1                   2                   3
+/// >  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+/// > +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// > |0 0|     STUN Message Type     |         Message Length        |
+/// > +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// > |                         Magic Cookie                          |
+/// > +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// > |                                                               |
+/// > |                     Transaction ID (96 bits)                  |
+/// > |                                                               |
+/// > +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// >
+/// >             Figure 2: Format of STUN Message Header
+/// > ```
+/// >
+/// > The most significant 2 bits of every STUN message MUST be zeroes.
+/// > This can be used to differentiate STUN packets from other protocols
+/// > when STUN is multiplexed with other protocols on the same port.
+/// >
+/// > The message type defines the message class (request, success
+/// > response, failure response, or indication) and the message method
+/// > (the primary function) of the STUN message.  Although there are four
+/// > message classes, there are only two types of transactions in STUN:
+/// > request/response transactions (which consist of a request message and
+/// > a response message) and indication transactions (which consist of a
+/// > single indication message).  Response classes are split into error
+/// > and success responses to aid in quickly processing the STUN message.
+/// >
+/// > The message type field is decomposed further into the following structure:
+/// >
+/// > ```text
+/// >  0                 1
+/// >  2  3  4 5 6 7 8 9 0 1 2 3 4 5
+/// > +--+--+-+-+-+-+-+-+-+-+-+-+-+-+
+/// > |M |M |M|M|M|C|M|M|M|C|M|M|M|M|
+/// > |11|10|9|8|7|1|6|5|4|0|3|2|1|0|
+/// > +--+--+-+-+-+-+-+-+-+-+-+-+-+-+
+/// >
+/// > Figure 3: Format of STUN Message Type Field
+/// > ```
+/// >
+/// > Here the bits in the message type field are shown as most significant
+/// > (M11) through least significant (M0).  M11 through M0 represent a 12-
+/// > bit encoding of the method.  C1 and C0 represent a 2-bit encoding of
+/// > the class.  A class of 0b00 is a request, a class of 0b01 is an
+/// > indication, a class of 0b10 is a success response, and a class of
+/// > 0b11 is an error response.  This specification defines a single
+/// > method, Binding.  The method and class are orthogonal, so that for
+/// > each method, a request, success response, error response, and
+/// > indication are possible for that method.  Extensions defining new
+/// > methods MUST indicate which classes are permitted for that method.
+/// >
+/// > For example, a Binding request has class=0b00 (request) and
+/// > method=0b000000000001 (Binding) and is encoded into the first 16 bits
+/// > as 0x0001.  A Binding response has class=0b10 (success response) and
+/// > method=0b000000000001, and is encoded into the first 16 bits as 0x0101.
+/// >
+/// > > Note: This unfortunate encoding is due to assignment of values in
+/// > > [RFC3489] that did not consider encoding Indications, Success, and
+/// > > Errors using bit fields.
+/// >
+/// > The magic cookie field MUST contain the fixed value 0x2112A442 in
+/// > network byte order.  In RFC 3489 [RFC3489], this field was part of
+/// > the transaction ID; placing the magic cookie in this location allows
+/// > a server to detect if the client will understand certain attributes
+/// > that were added in this revised specification.  In addition, it aids
+/// > in distinguishing STUN packets from packets of other protocols when
+/// > STUN is multiplexed with those other protocols on the same port.
+/// >
+/// > The transaction ID is a 96-bit identifier, used to uniquely identify
+/// > STUN transactions.  For request/response transactions, the
+/// > transaction ID is chosen by the STUN client for the request and
+/// > echoed by the server in the response.  For indications, it is chosen
+/// > by the agent sending the indication.  It primarily serves to
+/// > correlate requests with responses, though it also plays a small role
+/// > in helping to prevent certain types of attacks.  The server also uses
+/// > the transaction ID as a key to identify each transaction uniquely
+/// > across all clients.  As such, the transaction ID MUST be uniformly
+/// > and randomly chosen from the interval 0 .. 2**96-1, and SHOULD be
+/// > cryptographically random.  Resends of the same request reuse the same
+/// > transaction ID, but the client MUST choose a new transaction ID for
+/// > new transactions unless the new request is bit-wise identical to the
+/// > previous request and sent from the same transport address to the same
+/// > IP address.  Success and error responses MUST carry the same
+/// > transaction ID as their corresponding request.  When an agent is
+/// > acting as a STUN server and STUN client on the same port, the
+/// > transaction IDs in requests sent by the agent have no relationship to
+/// > the transaction IDs in requests received by the agent.
+/// >
+/// > The message length MUST contain the size, in bytes, of the message
+/// > not including the 20-byte STUN header.  Since all STUN attributes are
+/// > padded to a multiple of 4 bytes, the last 2 bits of this field are
+/// > always zero.  This provides another way to distinguish STUN packets
+/// > from packets of other protocols.
+/// >
+/// > Following the STUN fixed portion of the header are zero or more
+/// > attributes.  Each attribute is TLV (Type-Length-Value) encoded.  The
+/// > details of the encoding, and of the attributes themselves are given
+/// > in Section 15.
+/// >
+/// > [RFC 5389 -- 6. STUN Message Structure](https://tools.ietf.org/html/rfc5389#section-6)
 #[derive(Debug, Clone)]
 pub struct RawMessage {
     class: Class,
@@ -15,6 +135,7 @@ pub struct RawMessage {
     attributes: Vec<RawAttribute>,
 }
 impl RawMessage {
+    /// Makes a new `RawMessage` instance.
     pub fn new(class: Class,
                method: U12,
                transaction_id: TransactionId,
@@ -27,23 +148,35 @@ impl RawMessage {
             attributes: attributes,
         }
     }
+
+    /// Returns the class of this message.
     pub fn class(&self) -> Class {
         self.class
     }
+
+    /// Returns the method of this message.
     pub fn method(&self) -> U12 {
         self.method
     }
+
+    /// Returns the transaction ID of this message.
     pub fn transaction_id(&self) -> &TransactionId {
         &self.transaction_id
     }
+
+    /// Returns the attributes of this message.
     pub fn attributes(&self) -> &[RawAttribute] {
         &self.attributes
     }
+
+    /// Converts this message to the corresponding binary format.
     pub fn to_bytes(&self) -> Vec<u8> {
         let mut buf = Vec::new();
         self.write_to(&mut buf).expect("must succeed");
         buf
     }
+
+    /// Writes the binary format of this message to `writer`.
     pub fn write_to<W: Write>(&self, writer: &mut W) -> Result<()> {
         let mut temp_writer = io::Cursor::new(vec![0; 20]);
         let message_type = Type {
@@ -69,6 +202,8 @@ impl RawMessage {
         track_try!(writer.write_all(&buf));
         Ok(())
     }
+
+    /// Reads the binary format of a message from `reader` and returns the resulting message.
     pub fn read_from<R: Read>(reader: &mut R) -> Result<Self> {
         let message_type = track_try!(reader.read_u16be());
         let message_type = track_try!(Type::from_u16(message_type));
@@ -99,125 +234,88 @@ impl RawMessage {
                            attrs))
     }
 
-    pub fn try_into_request<M, A>(mut self) -> Result<Request<M, A>>
-        where M: Method,
-              A: Attribute
-    {
-        use std::mem;
+    /// Tries to convert into the corresponding request message.
+    pub fn try_into_request<M: Method, A: Attribute>(self) -> Result<Request<M, A>> {
         track_assert_eq!(self.class, Class::Request, ErrorKind::Other);
-        let method = track_try!(M::from_u12(self.method).ok_or(ErrorKind::Other));
-        let mut attrs = mem::replace(&mut self.attributes, Vec::new());
-        let mut raw_attrs = Vec::new();
-        for a in attrs {
-            let raw = track_try!(A::try_from_raw(&a, &self));
-            raw_attrs.push(raw);
-            self.attributes.push(a);
-        }
+        let (method, transaction_id, attrs) = track_try!(self.try_into());
         Ok(Request {
             method: method,
-            transaction_id: self.transaction_id,
-            attributes: raw_attrs,
-        })
-    }
-    pub fn try_into_indication<M, A>(self) -> Result<Indication<M, A>>
-        where M: Method,
-              A: Attribute
-    {
-        track_assert_eq!(self.class, Class::Indication, ErrorKind::Other);
-        let method = track_try!(M::from_u12(self.method).ok_or(ErrorKind::Other));
-        let mut attrs = Vec::new();
-        for a in self.attributes.iter() {
-            let a = track_try!(A::try_from_raw(&a, &self));
-            attrs.push(a);
-        }
-        Ok(Indication {
-            method: method,
-            transaction_id: self.transaction_id,
+            transaction_id: transaction_id,
             attributes: attrs,
         })
     }
-    pub fn try_into_response<M, A>(self) -> Result<Response<M, A>>
-        where M: Method,
-              A: Attribute
-    {
-        track_assert!(self.class == Class::SuccessResponse || self.class == Class::ErrorResponse,
+
+    /// Tries to convert into the corresponding indication message.
+    pub fn try_into_indication<M: Method, A: Attribute>(self) -> Result<Indication<M, A>> {
+        track_assert_eq!(self.class, Class::Indication, ErrorKind::Other);
+        let (method, transaction_id, attrs) = track_try!(self.try_into());
+        Ok(Indication {
+            method: method,
+            transaction_id: transaction_id,
+            attributes: attrs,
+        })
+    }
+
+    /// Tries to convert into the corresponding responses message.
+    pub fn try_into_response<M: Method, A: Attribute>(self) -> Result<Response<M, A>> {
+        let class = self.class;
+        track_assert!(class == Class::SuccessResponse || class == Class::ErrorResponse,
                       ErrorKind::Other);
-        let method = track_try!(M::from_u12(self.method).ok_or(ErrorKind::Other));
-        let mut attrs = Vec::new();
-        for a in self.attributes.iter() {
-            let a = track_try!(A::try_from_raw(&a, &self));
-            attrs.push(a);
-        }
-        if self.class == Class::SuccessResponse {
+        let (method, transaction_id, attrs) = track_try!(self.try_into());
+        if class == Class::SuccessResponse {
             Ok(Ok(SuccessResponse {
                 method: method,
-                transaction_id: self.transaction_id,
+                transaction_id: transaction_id,
                 attributes: attrs,
             }))
         } else {
-            panic!()
-            // TODO: handle error code attribute
-            // Ok(Err(ErrorResponse {
-            //     method: method,
-            //     transaction_id: self.transaction_id,
-            //     attributes: Vec::new(),
-            // }))
+            Ok(Err(ErrorResponse {
+                method: method,
+                transaction_id: transaction_id,
+                attributes: attrs,
+            }))
         }
     }
-    pub fn try_from_request<M, A>(from: Request<M, A>) -> Result<Self>
-        where M: Method,
-              A: Attribute
-    {
+
+    /// Tries to convert from `Request` to `RawMessage`.
+    pub fn try_from_request<M: Method, A: Attribute>(from: Request<M, A>) -> Result<Self> {
         track_err!(Self::try_from(Class::Request,
                                   from.method(),
                                   *from.transaction_id(),
                                   from.attributes()))
     }
-    pub fn try_from_indication<M, A>(from: Indication<M, A>) -> Result<Self>
-        where M: Method,
-              A: Attribute
-    {
+
+    /// Tries to convert from `Indication` to `RawMessage`.
+    pub fn try_from_indication<M: Method, A: Attribute>(from: Indication<M, A>) -> Result<Self> {
         track_err!(Self::try_from(Class::Indication,
                                   from.method(),
                                   *from.transaction_id(),
                                   from.attributes()))
     }
-    pub fn try_from_response<M, A>(from: Response<M, A>) -> Result<Self>
-        where M: Method,
-              A: Attribute
-    {
-        match from {
-            Err(f) => Self::try_from_error_response(f),
-            Ok(f) => Self::try_from_success_response(f),
-        }
-    }
-    pub fn try_from_success_response<M, A>(from: SuccessResponse<M, A>) -> Result<Self>
-        where M: Method,
-              A: Attribute
-    {
-        track_err!(Self::try_from(Class::SuccessResponse,
-                                  from.method(),
-                                  *from.transaction_id(),
-                                  from.attributes()))
-    }
-    pub fn try_from_error_response<M, A>(from: ErrorResponse<M, A>) -> Result<Self>
-        where M: Method,
-              A: Attribute
-    {
-        track_err!(Self::try_from(Class::ErrorResponse,
-                                  from.method(),
-                                  *from.transaction_id(),
-                                  from.attributes()))
+
+    /// Tries to convert from `Response` to `RawMessage`.
+    pub fn try_from_response<M: Method, A: Attribute>(from: Response<M, A>) -> Result<Self> {
+        track_err!(match from {
+            Ok(m) => {
+                Self::try_from(Class::SuccessResponse,
+                               m.method(),
+                               *m.transaction_id(),
+                               m.attributes())
+            }
+            Err(m) => {
+                Self::try_from(Class::ErrorResponse,
+                               m.method(),
+                               *m.transaction_id(),
+                               m.attributes())
+            }
+        })
     }
 
-    fn try_from<M, A>(class: Class,
-                      method: &M,
-                      transaction_id: TransactionId,
-                      attributes: &[A])
-                      -> Result<Self>
-        where M: Method,
-              A: Attribute
-    {
+    fn try_from<M: Method, A: Attribute>(class: Class,
+                                         method: &M,
+                                         transaction_id: TransactionId,
+                                         attributes: &[A])
+                                         -> Result<Self> {
         let mut m = RawMessage::new(class, method.as_u12(), transaction_id, Vec::new());
         for attr in attributes {
             let raw_attr = track_try!(attr.try_to_raw(&m));
@@ -225,8 +323,38 @@ impl RawMessage {
         }
         Ok(m)
     }
+    fn try_into<M: Method, A: Attribute>(mut self) -> Result<(M, TransactionId, Vec<A>)> {
+        let method = track_try!(M::from_u12(self.method).ok_or(ErrorKind::Other));
+        let attrs_len = self.attributes.len();
+        let src_attrs = mem::replace(&mut self.attributes, Vec::with_capacity(attrs_len));
+        let mut dst_attrs = Vec::with_capacity(attrs_len);
+        for a in src_attrs {
+            let raw = track_try!(A::try_from_raw(&a, &self));
+            dst_attrs.push(raw);
+            self.attributes.push(a);
+        }
+        Ok((method, self.transaction_id, dst_attrs))
+    }
 }
 
+/// The class of a message.
+///
+/// > The class indicates whether this is a **request**, a **success response**,
+/// > an **error response**, or an **indication**.
+/// >
+/// > [RFC 5389 -- 3. Overview of Operation](https://tools.ietf.org/html/rfc5389#section-3)
+///
+/// An instance of `Class` can be casted to the corresponding `u8` value.
+///
+/// ```
+/// use rustun::message::Class;
+///
+/// assert_eq!(Class::Request as u8, 0b00);
+/// assert_eq!(Class::Indication as u8, 0b01);
+/// assert_eq!(Class::SuccessResponse as u8, 0b10);
+/// assert_eq!(Class::ErrorResponse as u8, 0b11);
+/// ```
+#[allow(missing_docs)]
 #[derive(Debug, Clone, Copy, PartialOrd, Ord, PartialEq, Eq, Hash)]
 pub enum Class {
     Request = 0b00,
@@ -235,6 +363,24 @@ pub enum Class {
     ErrorResponse = 0b11,
 }
 impl Class {
+    /// Returns a `Class` instance which is corresponding to `value`.
+    ///
+    /// > A class of `0b00` is a request, a class of `0b01` is an
+    /// > indication, a class of `0b10` is a success response, and a class of
+    /// > `0b11` is an error response.
+    /// >
+    /// > [RFC 5389 -- 6. STUN Message Structure](https://tools.ietf.org/html/rfc5389#section-6)
+    ///
+    /// If no such instance exists, this will return `None`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use rustun::message::Class;
+    ///
+    /// assert_eq!(Class::from_u8(0), Some(Class::Request));
+    /// assert_eq!(Class::from_u8(9), None);
+    /// ```
     pub fn from_u8(value: u8) -> Option<Self> {
         match value {
             0b00 => Some(Class::Request),
@@ -252,19 +398,6 @@ struct Type {
     pub method: U12,
 }
 impl Type {
-    /// TODO:
-    ///
-    /// ```text
-    /// 0                 1
-    /// 2  3  4 5 6 7 8 9 0 1 2 3 4 5
-    ///
-    /// +--+--+-+-+-+-+-+-+-+-+-+-+-+-+
-    /// |M |M |M|M|M|C|M|M|M|C|M|M|M|M|
-    /// |11|10|9|8|7|1|6|5|4|0|3|2|1|0|
-    /// +--+--+-+-+-+-+-+-+-+-+-+-+-+-+
-    ///
-    /// Figure 3: Format of STUN Message Type Field
-    /// ```
     pub fn as_u16(&self) -> u16 {
         let class = self.class as u16;
         let method = self.method.as_u12().as_u16();
