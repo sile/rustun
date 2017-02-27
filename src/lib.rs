ecd207110c59e655d234c4fac4c948d50e6305ad
Takeru Ohta <phjgt308@gmail.com>
2017-02-27 19:08:20 +0900

    Add crate level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
+//! Asynchronous implementation of STUN [[RFC 5389](https://tools.ietf.org/html/rfc5389)]
+//! server and client.
+//!
+//! # Examples
+//!
+//! A client-side example that issues a Binding request:
+//!
+//! ```no_run
+//! extern crate fibers;
+//! extern crate rustun;
+//!
+//! use fibers::{Executor, InPlaceExecutor, Spawn};
+//! use rustun::{Method, Client};
+//! use rustun::client::UdpClient;
+//! use rustun::rfc5389;
+//!
+//! fn main() {
+//!     let server_addr = "127.0.0.1:3478".parse().unwrap();
+//!     let mut executor = InPlaceExecutor::new().unwrap();
+//!
+//!     let mut client = UdpClient::new(&executor.handle(), server_addr);
+//!     let request = rfc5389::methods::Binding.request::<rfc5389::Attribute>();
+//!     let future = client.call(request);
+//!
+//!     let monitor = executor.spawn_monitor(future);
+//!     match executor.run_fiber(monitor).unwrap() {
+//!         Ok(v) => println!("SUCCEEDE: {:?}", v),
+//!         Err(e) => println!("ERROR: {}", e),
+//!     }
+//! }
+//! ```
+#![warn(missing_docs)]
+
 #[macro_use]
 extern crate slog;
 extern crate rand;
