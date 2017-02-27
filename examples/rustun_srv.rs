69dcb49d780ea09321422f7c0b922bac3f248e3d
Takeru Ohta <phjgt308@gmail.com>
2017-02-27 18:35:26 +0900

    Rewrite built-in handlers

--- examples/rustun_srv.rs
+++ examples/rustun_srv.rs
@@ -3,14 +3,13 @@ extern crate clap;
 extern crate slog;
 extern crate slog_term;
 extern crate fibers;
-extern crate futures;
 extern crate rustun;
 
 use clap::{App, Arg};
 use slog::{Logger, DrainExt, Record, LevelFilter};
 use fibers::{Executor, InPlaceExecutor, Spawn};
 use rustun::server::UdpServer;
-use rustun::rfc5389::handlers::DefaultMessageHandler;
+use rustun::rfc5389::handlers::BindingHandler;
 
 fn main() {
     let matches = App::new("rustun_srv")
@@ -26,14 +25,14 @@ fn main() {
     let addr = format!("0.0.0.0:{}", port).parse().expect("Invalid UDP address");
 
     let place_fn = |info: &Record| format!("{}:{}", info.module(), info.line());
-    let logger = Logger::root(LevelFilter::new(slog_term::streamer().build(), slog::Level::Debug)
+    let logger = Logger::root(LevelFilter::new(slog_term::streamer().build(), slog::Level::Info)
                                   .fuse(),
                               o!("place" => place_fn));
 
     let mut executor = InPlaceExecutor::new().unwrap();
     let spawner = executor.handle();
     let monitor = executor.spawn_monitor(UdpServer::new(addr)
-        .start(spawner.boxed(), DefaultMessageHandler::with_logger(logger)));
+        .start(spawner.boxed(), BindingHandler::with_logger(logger)));
     let result = executor.run_fiber(monitor).unwrap();
     println!("RESULT: {:?}", result);
 }
--- src/rfc5389/handlers.rs
+++ src/rfc5389/handlers.rs
@@ -1,40 +1,43 @@
+//! `HandleMessage` trait implementations.
 use std::net::SocketAddr;
 use slog::{self, Logger};
 use futures::{self, Future, BoxFuture};
 
 use {Error, HandleMessage};
 use message::{Request, Response, Indication};
+use rfc5389;
+use rfc5389::attributes::XorMappedAddress;
 
+/// A `HandleMessage` implementation which only handle `Binding` method.
 #[derive(Debug)]
-pub struct DefaultMessageHandler {
+pub struct BindingHandler {
     logger: Logger,
 }
-impl DefaultMessageHandler {
+impl BindingHandler {
+    /// Makes a new `BindingHandler` instance.
     pub fn new() -> Self {
-        DefaultMessageHandler { logger: Logger::root(slog::Discard, o!()) }
+        BindingHandler { logger: Logger::root(slog::Discard, o!()) }
     }
+
+    /// Makes a new `BindingHandler` instance with the speficied logger.
+    ///
+    /// The logger is used for logging errors that ocurred while handling transactions.
     pub fn with_logger(logger: Logger) -> Self {
-        DefaultMessageHandler { logger: logger }
+        BindingHandler { logger: logger }
     }
 }
-impl HandleMessage for DefaultMessageHandler {
-    type Method = super::Method;
-    type Attribute = super::Attribute;
+impl HandleMessage for BindingHandler {
+    type Method = rfc5389::methods::Binding;
+    type Attribute = rfc5389::Attribute;
     type HandleCall = BoxFuture<Response<Self::Method, Self::Attribute>, ()>;
     type HandleCast = BoxFuture<(), ()>;
     fn handle_call(&mut self,
                    client: SocketAddr,
                    request: Request<Self::Method, Self::Attribute>)
                    -> Self::HandleCall {
-        match *request.method() {
-            super::Method::Binding => {
-                let mut response = request.into_success_response();
-                response.add_attribute(
-                    super::Attribute::XorMappedAddress(
-                        super::attributes::XorMappedAddress::new(client)));
-                futures::finished(Ok(response)).boxed()
-            }
-        }
+        let mut response = request.into_success_response();
+        response.add_attribute(XorMappedAddress::new(client));
+        futures::finished(Ok(response)).boxed()
     }
     fn handle_cast(&mut self,
                    _client: SocketAddr,
